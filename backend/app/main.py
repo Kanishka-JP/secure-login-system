28caf4756b96cc3f2285651edcd2b539ac5331b0
Kanishka <kanishkajoseph3@gmail.com>
2026-01-13 16:14:36 +0530

    updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",   # React (Vite)
+        "http://localhost:5173", "https://secure-login-system.vercel.app"  # React (Vite)
     ],
     allow_credentials=True,
     allow_methods=["*"],
