13ea0fa1e2eee2f5a25d59613488057d6ad4a89f
Kanishka <kanishkajoseph3@gmail.com>
2026-01-13 13:54:58 +0530

    updated database

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os, json
 
-cred = credentials.Certificate("firebase-key.json")
+cred_json = json.loads(os.getenv("FIREBASE_CREDENTIALS_JSON"))
+cred = credentials.Certificate(cred_json)
+
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
