b01e514d871a2002939a52ffcde6827c3e0eeb7d
Kanishka <kanishkajoseph3@gmail.com>
2026-01-13 14:56:35 +0530

    updated database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os, json
+import os
+import json
 
-cred_json = json.loads(os.getenv("FIREBASE_CREDENTIALS_JSON"))
-cred = credentials.Certificate(cred_json)
+# Prevent double initialization
+if not firebase_admin._apps:
+    firebase_credentials = os.getenv("FIREBASE_CREDENTIALS_JSON")
 
-firebase_admin.initialize_app(cred)
+    if not firebase_credentials:
+        raise RuntimeError("FIREBASE_CREDENTIALS_JSON not set")
+
+    cred_dict = json.loads(firebase_credentials)
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
+
 db = firestore.client()
-
